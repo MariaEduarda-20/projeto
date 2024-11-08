1b9aa944805531c375231aa8cfd601928ac45cd4
Maria Eduarda <dudacardosoo635@gmail.com>
2024-11-08 15:15:19 -0300

    change background based on media queries

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,15 +1,16 @@
-function toggleNode() {
+function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  //pegar a tag img
+  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  //substituir a imagem
-  if (html.classlist.contains("light")) {
-    //se tiver light mode,adicionar a imagem light
-    img.setAttribute("src", "./assets/golden_retriever.png")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    //se tiver sem light mode,manter a imagem normal
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/ golden_retriever-p.webp")
   }
 }
